df0d8ca57dfc6c9e3b6a28f5b84f1057181cc4b1
ferric <ferric-sol@ferric.net>
2025-03-03 06:43:52 -0600

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove the custom webpack config since Next.js has built-in CSS support
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'standalone'
+  distDir: '.next'
 };
 
 module.exports = nextConfig; 
